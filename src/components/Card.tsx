550d84aaeab1a60cc2c8c4274f0e68a974a84472
natsumi <aka.723n@gmail.com>
2022-06-01 14:22:49 +0900

    style: css調整

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Card = () => {
   return(
-    <div className="flex box bg-teal-100 w-96 justify-between p-9 rounded-md ">
+    <div className="flex box bg-teal-100 w-96 justify-between p-9 rounded-md mx-auto my-8">
     Card
     </div>
   );
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const InputForm = () => {
   return(
-    <div className="flex box bg-teal-300 w-96 justify-between p-5 rounded-md ">
-      <input type="text" placeholder="TODOを入力" className="bg-white"/>
-      <button className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-100">閉じる</button>
+    <div className="flex box bg-teal-300 w-96 justify-between px-5 rounded-md mx-auto	">
+      <input type="text" placeholder="TODOを入力" className="bg-white px-8 rounded-full"/>
+      <button className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-100">作成</button>
     </div>
   );
 }
