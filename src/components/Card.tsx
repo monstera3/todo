69142d494d155e7934228a537bee209a2991b7a2
natsumi <aka.723n@gmail.com>
2022-06-02 20:30:48 +0900

    style: 全体の色をgrayに変更

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 export const Card = (props:any) => {
   return(
     <>
-        <p className="bg-teal-100 w-96 justify-between  rounded-md mx-auto my-8 text-center">未完了のTODO</p>
+        <p className=" w-96 justify-between  mx-auto my-8 text-center">未完了のTODO</p>
         {props.tasks.map((tasks:string) => {
           return(
             <div key={tasks}>
-              <div className="flex box bg-teal-100 w-96 justify-between p-9 rounded-md mx-auto my-8">
+              <div className="flex box w-96 justify-between p-9 rounded-md mx-auto my-8 border border-gray-40 ">
               <div>{tasks}</div>
               <button >完了</button>
               </div>
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const InputForm = (props:any) => {
   const {inputText,onChange,onClick} = props;
   return(
-    <div className="flex box  w-96 justify-between px-5 py-2 rounded-full mx-auto	border-2 border-indigo-600 m-8">
+    <div className="flex box  w-96 justify-between px-5 py-2 rounded-md mx-auto	border border-gray-300 m-8 shadow-lg shadow-gray-300/50 ">
       <input value={inputText} onChange={onChange} type="text" placeholder="TODOを入力" className=" px-8 rounded-full"/>
-      <button onClick={onClick} className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-100">作成</button>
+      <button onClick={onClick} className=" py-2 px-4 rounded hover:bg-gray-300">作成</button>
     </div>
   );
 }
