7cad692dbb9223a6f3e60bac56482bc6773ec434
natsumi <aka.723n@gmail.com>
2022-06-02 20:16:59 +0900

    style: InputFormのcss変更

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export const InputForm = (props:any) => {
   const {inputText,onChange,onClick} = props;
   return(
-    <div className="flex box bg-teal-300 w-96 justify-between px-5 rounded-md mx-auto	">
-      <input value={inputText} onChange={onChange} type="text" placeholder="TODOを入力" className="bg-white px-8 rounded-full"/>
+    <div className="flex box  w-96 justify-between px-5 py-2 rounded-full mx-auto	border-2 border-indigo-600 m-8">
+      <input value={inputText} onChange={onChange} type="text" placeholder="TODOを入力" className=" px-8 rounded-full"/>
       <button onClick={onClick} className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-100">作成</button>
     </div>
   );
