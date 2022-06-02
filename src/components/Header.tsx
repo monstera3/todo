9865f611a7bcc1b3c5b9ff9eedbe851173b3ff3c
natsumi <aka.723n@gmail.com>
2022-06-02 20:46:41 +0900

    style: shadow-mdに変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
         <h1 className="text-3xl font-bold">
           TODO
         </h1>
-        <div className="flex box  w-96 justify-between px-5 py-2 rounded-md mx-auto	border border-gray-300  shadow-lg shadow-gray-300/50 ">
+        <div className="flex box  w-96 justify-between px-5 py-2 rounded-md mx-auto	border border-gray-300  shadow-md shadow-gray-300/50 ">
           <input type="text" placeholder="検索" className=" px-8 rounded-full"/>
         </div>
         
