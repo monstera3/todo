7c8bae34c8574e1fe733df5feb28f3121f28f86d
natsumi <aka.723n@gmail.com>
2022-06-03 20:15:30 +0900

    feat: add icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css';
 import { Header } from './components/Header';
 import { InputForm } from './components/InputForm';
 import { Card } from './components/Card';
+import { MdOutlineLightbulb } from 'react-icons/md';
+import { BiBell, BiTag, BiTrash } from 'react-icons/bi';
+import { BsPencil } from 'react-icons/bs';
+import { RiInboxArchiveLine } from 'react-icons/ri';
 
 function App() {
   const [inputText,setInputText] = useState('');
@@ -33,12 +37,13 @@ function App() {
       <Header toggleMenuIsOpen={toggleMenuIsOpen}/>
       <div className="flex flex-row w-screen">
         <div className={drawerWidth()}>
-          <div>メモ</div>
-          <div>リマインダー</div>
-          <div>メモ</div>
-          <div>料理</div>
-          <div>ラベルの編集</div>
-          <div>アーカイブ</div>
+          <div><MdOutlineLightbulb size='1.5rem'/>メモ</div>
+          <div><BiBell size='1.5rem'/>リマインダー</div>
+          <div><BiTag size='1.5rem'/>メモ</div>
+          <div><BiTag size='1.5rem'/>料理</div>
+          <div><BsPencil size='1.5rem'/>ラベルの編集</div>
+          <div><RiInboxArchiveLine size='1.5rem'/>アーカイブ</div>
+          <div><BiTrash size='1.5rem'/>ゴミ箱</div>
         </div>
       <div className="flex flex-col w-full justify-items-center">
       <InputForm
