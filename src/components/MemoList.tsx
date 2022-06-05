a89479119891a9334a7503779f0238e6828993aa
natsumi <aka.723n@gmail.com>
2022-06-05 16:47:35 +0900

    feat(MemoList): displayIsListに応じてgrid-colsを変更

--- src/components/MemoList.tsx
+++ src/components/MemoList.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 
 
 export const MemoList = (props: {memoList: string[], displayIsList: boolean}) => {
+  const gridCols = () => {
+    return props.displayIsList ? 'grid-cols-1' : 'grid-cols-3';
+  }
+
   return(
     <div>
       <p>固定済み</p>
-      {props.memoList.map((memo:string) => {
-        return(
-          <div key={memo}>
-            <div className="flex box w-96 justify-between p-9 rounded-md mx-auto my-8 border border-gray-40 ">
-              <div>{memo}</div>
-              <button >完了</button>
+      <div className={'grid ' + gridCols()}>
+        {props.memoList.map((memo:string) => {
+          return(
+            <div key={memo}>
+              <div className="flex box justify-between p-9 rounded-md mx-auto my-8 border border-gray-40 ">
+                <div>{memo}</div>
+                <button >完了</button>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
