5008fa39e99696ca158cce7050888f8d4eba7463
natsumi <aka.723n@gmail.com>
2022-06-05 13:25:56 +0900

    refactor: extract Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,22 @@ import { BiBell, BiTag, BiTrash } from 'react-icons/bi';
 import { BsPencil } from 'react-icons/bs';
 import { RiInboxArchiveLine } from 'react-icons/ri';
 
+const Drawer = (props:{menuIsOpen:boolean}) => {
+  const drawerWidth = () => {
+    return props.menuIsOpen ? "w-40" : "w-40 text-transparent ";
+  }
+  return(
+    <div >
+      <div className='flex m-4'><MdOutlineLightbulb className='mr-4' size='1.5rem'/><p className={drawerWidth()}>メモ</p></div>
+      <div className='flex m-4'><BiBell className='mr-4' size='1.5rem'/><p className={drawerWidth()}>リマインダー</p></div>
+      <div className='flex m-4'><BiTag className='mr-4' size='1.5rem'/><p className={drawerWidth()}>料理</p></div>
+      <div className='flex m-4'><BsPencil className='mr-4' size='1.5rem'/><p className={drawerWidth()}>ラベルの編集</p></div>
+      <div className='flex m-4'><RiInboxArchiveLine className='mr-4' size='1.5rem'/><p className={drawerWidth()}>アーカイブ</p></div>
+      <div className='flex m-4'><BiTrash className='mr-4' size='1.5rem'/><p className={drawerWidth()}>ゴミ箱</p></div>
+    </div>
+  );
+}
+
 function App() {
   const [inputText,setInputText] = useState('');
   const [taskList,setTaskList] = useState(['aa', 'bb']);
@@ -24,9 +40,7 @@ function App() {
     setInputText('');
   };
 
-  const drawerWidth = () => {
-    return menuIsOpen ? "w-40" : "w-40 text-transparent ";
-  }
+
 
 
   const toggleMenuIsOpen = () => {
@@ -37,14 +51,7 @@ function App() {
     <div className="App">
       <Header toggleMenuIsOpen={toggleMenuIsOpen}/>
       <div className="flex flex-row w-screen">
-        <div >
-          <div className='flex m-4'><MdOutlineLightbulb className='mr-4' size='1.5rem'/><p className={drawerWidth()}>メモ</p></div>
-          <div className='flex m-4'><BiBell className='mr-4' size='1.5rem'/><p className={drawerWidth()}>リマインダー</p></div>
-          <div className='flex m-4'><BiTag className='mr-4' size='1.5rem'/><p className={drawerWidth()}>料理</p></div>
-          <div className='flex m-4'><BsPencil className='mr-4' size='1.5rem'/><p className={drawerWidth()}>ラベルの編集</p></div>
-          <div className='flex m-4'><RiInboxArchiveLine className='mr-4' size='1.5rem'/><p className={drawerWidth()}>アーカイブ</p></div>
-          <div className='flex m-4'><BiTrash className='mr-4' size='1.5rem'/><p className={drawerWidth()}>ゴミ箱</p></div>
-        </div>
+        <Drawer menuIsOpen={menuIsOpen}/>
         <div className="flex flex-col w-full justify-items-center">
           <InputForm
             inputText={inputText} onChange={onChangeInputText} onClick={onClickAdd}/>
