fa87cb2c3b97c312df58ad7cb2b200f974a799a5
natsumi <aka.723n@gmail.com>
2022-06-05 19:00:02 +0900

    feat(MemoList): displayIsListに応じてlistWidthを変更

--- src/components/MemoList.tsx
+++ src/components/MemoList.tsx
@@ -6,14 +6,19 @@ export const MemoList = (props: {memoList: string[], displayIsList: boolean}) =>
     return props.displayIsList ? 'grid-cols-1' : 'grid-cols-3';
   }
 
+  const listWidth = () => {
+    return props.displayIsList ? 'w-2/3 ' : 'w-full';
+
+  }
+
   return(
     <div>
       <p>固定済み</p>
-      <div className={'grid ' + gridCols()}>
+      <div className={ listWidth() +' grid gap-4 mr-4 ' + gridCols()  }>
         {props.memoList.map((memo:string) => {
           return(
             <div key={memo}>
-              <div className="flex box justify-between p-9 rounded-md mx-auto my-8 border border-gray-40 ">
+              <div className="flex justify-between p-9 rounded-md mx-auto my-8 border border-gray-40 ">
                 <div>{memo}</div>
                 <button >完了</button>
               </div>
