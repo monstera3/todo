3c46c1065f86363af79b8cda9a59dda5a1ef0cc2
natsumi <aka.723n@gmail.com>
2022-06-05 13:45:59 +0900

    refactor: extract menuList

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { InputForm } from './components/InputForm';
@@ -8,18 +8,60 @@ import { BiBell, BiTag, BiTrash } from 'react-icons/bi';
 import { BsPencil } from 'react-icons/bs';
 import { RiInboxArchiveLine } from 'react-icons/ri';
 
+type Menu = {
+  key: string,
+  label: string,
+  icon: ReactElement,
+}
+
+const menuList: Menu[] = [
+  {
+    key: 'menu',
+    label: 'メニュー',
+    icon: (<MdOutlineLightbulb className='mr-4' size='1.5rem'/>),
+  },
+  {
+    key: 'reminder',
+    label: 'リマインダー',
+    icon: (<BiBell className='mr-4' size='1.5rem'/>),
+  },
+  {
+    key: 'cooking',
+    label: '料理',
+    icon: (<BiTag className='mr-4' size='1.5rem'/>),
+  },
+  {
+    key: 'editLabel',
+    label: 'ラベルの編集',
+    icon: (<BsPencil className='mr-4' size='1.5rem'/>),
+  },
+  {
+    key: 'archive',
+    label: 'アーカイブ',
+    icon: (<RiInboxArchiveLine className='mr-4' size='1.5rem'/>),
+  },
+  {
+    key: 'trashBox',
+    label: 'ゴミ箱',
+    icon: (<BiTrash className='mr-4' size='1.5rem'/>),
+  },
+];
+
+
 const Drawer = (props:{menuIsOpen:boolean}) => {
   const drawerWidth = () => {
     return props.menuIsOpen ? "w-40" : "w-40 text-transparent ";
   }
   return(
+
     <div >
-      <div className='flex m-4'><MdOutlineLightbulb className='mr-4' size='1.5rem'/><p className={drawerWidth()}>メモ</p></div>
-      <div className='flex m-4'><BiBell className='mr-4' size='1.5rem'/><p className={drawerWidth()}>リマインダー</p></div>
-      <div className='flex m-4'><BiTag className='mr-4' size='1.5rem'/><p className={drawerWidth()}>料理</p></div>
-      <div className='flex m-4'><BsPencil className='mr-4' size='1.5rem'/><p className={drawerWidth()}>ラベルの編集</p></div>
-      <div className='flex m-4'><RiInboxArchiveLine className='mr-4' size='1.5rem'/><p className={drawerWidth()}>アーカイブ</p></div>
-      <div className='flex m-4'><BiTrash className='mr-4' size='1.5rem'/><p className={drawerWidth()}>ゴミ箱</p></div>
+      {
+        menuList.map((menu:Menu)=>{
+          return(
+            <div key={menu.key} className='flex m-4'>{menu.icon}<p className={drawerWidth()}>{menu.label}</p></div>
+          )
+          })
+      }
     </div>
   );
 }
