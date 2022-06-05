e22cfdd068419bcd4c82723f998a129304b72db5
natsumi <aka.723n@gmail.com>
2022-06-05 14:12:02 +0900

    refactor: add Drawer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,10 @@
-import React, { ReactElement, useState } from 'react';
+import React, { useState } from 'react';
+import { Drawer } from './components/Drawer';
 import './App.css';
 import { Header } from './components/Header';
 import { InputForm } from './components/InputForm';
 import { Card } from './components/Card';
-import { MdOutlineLightbulb } from 'react-icons/md';
-import { BiBell, BiTag, BiTrash } from 'react-icons/bi';
-import { BsPencil } from 'react-icons/bs';
-import { RiInboxArchiveLine } from 'react-icons/ri';
 
-type Menu = {
-  key: string,
-  label: string,
-  icon: ReactElement,
-}
-
-const menuList: Menu[] = [
-  {
-    key: 'menu',
-    label: 'メニュー',
-    icon: (<MdOutlineLightbulb className='mr-4' size='1.5rem'/>),
-  },
-  {
-    key: 'reminder',
-    label: 'リマインダー',
-    icon: (<BiBell className='mr-4' size='1.5rem'/>),
-  },
-  {
-    key: 'cooking',
-    label: '料理',
-    icon: (<BiTag className='mr-4' size='1.5rem'/>),
-  },
-  {
-    key: 'editLabel',
-    label: 'ラベルの編集',
-    icon: (<BsPencil className='mr-4' size='1.5rem'/>),
-  },
-  {
-    key: 'archive',
-    label: 'アーカイブ',
-    icon: (<RiInboxArchiveLine className='mr-4' size='1.5rem'/>),
-  },
-  {
-    key: 'trashBox',
-    label: 'ゴミ箱',
-    icon: (<BiTrash className='mr-4' size='1.5rem'/>),
-  },
-];
-
-
-const Drawer = (props:{menuIsOpen:boolean}) => {
-
-  return(
-
-    <div >
-      {
-        menuList.map((menu:Menu)=>{
-          if(props.menuIsOpen){
-            return(
-              <div key={menu.key} className='flex m-4'>{menu.icon}<p className="w-40">{menu.label}</p></div>
-            )
-          }else {
-           return(
-              <div key={menu.key} className='flex m-4'>{menu.icon}</div>
-            )
-          }
-
-          })
-      }
-    </div>
-  );
-}
 
 function App() {
   const [inputText,setInputText] = useState('');
