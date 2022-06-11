2a61a4ba635610ddb5a9197b0b9e2825fca519c5
natsumi <aka.723n@gmail.com>
2022-06-11 17:04:20 +0900

    refactor: extract Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
-import { Drawer } from './components/templates/Drawer';
+import React from 'react';
 import './App.css';
-import { Header } from './components/templates/Header';
-import { InputForm } from './components/organisms/InputForm';
-import { MemoList } from './components/templates/MemoList';
 import { MemoDetail } from './components/pages/MemoDetail';
+import { Home } from './components/templates/Home';
 
 
 
@@ -12,67 +9,12 @@ export type Memo = {
   title: string
   body: string
 }
-function App(props: { memoList: Memo[] }) {
-  const [inputText,setInputText] = useState('');
-  const [inputTitle,setInputTitle] = useState('');
-  const [memoList,setMemoList] = useState<Memo[]>(props.memoList);
-  const [menuIsOpen, setMenuIsOpen] = useState(true);
-  const [displayIsList, setDisplayIsList] = useState<boolean>(false);
 
-  const onChangeInputText = (e:React.ChangeEvent<HTMLInputElement>) =>{
-    setInputText(e.target.value)
-  }
-
-  const onChangeInputTitle = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setInputTitle(e.target.value)
-  }
-  const onClickAdd = () => {
-    if (inputText === '' && inputTitle === '') return;
-    const newMemos = [...memoList, { title: inputTitle, body: inputText }];
-    setMemoList(newMemos);
-    updateStoredMemos(newMemos);
-    setInputText('');
-    setInputTitle('');
-  };
-
-  const onClickDelete = (index:number) => {
-    const newMemos = [...memoList];
-    newMemos.splice(index,1);
-    setMemoList(newMemos);
-    updateStoredMemos(newMemos);
-  }
-
-  const toggleDisplayIsList = () => {
-    setDisplayIsList(!displayIsList);
-  }
-
-
-  const toggleMenuIsOpen = () => {
-    setMenuIsOpen(!menuIsOpen);
-  }
-
-  //ローカルストレージのキーのstoredMemosに追加される
-  const updateStoredMemos = (updatedMemos: Memo[]) => {
-    localStorage.setItem('storedMemos', JSON.stringify(updatedMemos));
-  }
-
+export const App = (props: {memoList:Memo[]})=>{
   return (
     <div className="App">
-      <Header toggleMenuIsOpen={toggleMenuIsOpen} toggleDisplayIsList={toggleDisplayIsList} displayIsList={displayIsList}/>
-      <div className="flex flex-row w-screen">
-        <Drawer menuIsOpen={menuIsOpen}/>
-        <div className="flex flex-col w-full justify-items-center">
-          <InputForm
-            inputText={inputText} onChangeInputText={onChangeInputText} onClick={onClickAdd}
-            inputTitle={inputTitle} onChangeInputTitle={onChangeInputTitle}
-          />
-          <MemoList
-            memoList={memoList}
-            displayIsList={displayIsList}
-            onClickDelete={onClickDelete}/>
-          <MemoDetail />
-        </div>
-      </div>
+      <Home memoList={props.memoList}/>
+      <MemoDetail />
     </div>
   );
 }
---
+++ src/components/templates/Home.tsx
@@ -0,0 +1,74 @@
+import { Header } from './Header';
+import { Drawer } from './Drawer';
+import { InputForm } from '../organisms/InputForm';
+import { MemoList } from './MemoList';
+import { MemoDetail } from '../pages/MemoDetail';
+import React, { useState } from 'react';
+import { Memo } from '../../App';
+
+
+  export const Home = (props: { memoList: Memo[] }) => {
+    const [inputText,setInputText] = useState('');
+    const [inputTitle,setInputTitle] = useState('');
+    const [memoList,setMemoList] = useState<Memo[]>(props.memoList);
+    const [menuIsOpen, setMenuIsOpen] = useState(true);
+    const [displayIsList, setDisplayIsList] = useState<boolean>(false);
+
+    const onChangeInputText = (e:React.ChangeEvent<HTMLInputElement>) =>{
+      setInputText(e.target.value)
+    }
+
+    const onChangeInputTitle = (e:React.ChangeEvent<HTMLInputElement>) => {
+      setInputTitle(e.target.value)
+    }
+    const onClickAdd = () => {
+      if (inputText === '' && inputTitle === '') return;
+      const newMemos = [...memoList, { title: inputTitle, body: inputText }];
+      setMemoList(newMemos);
+      updateStoredMemos(newMemos);
+      setInputText('');
+      setInputTitle('');
+    };
+
+    const onClickDelete = (index:number) => {
+      const newMemos = [...memoList];
+      newMemos.splice(index,1);
+      setMemoList(newMemos);
+      updateStoredMemos(newMemos);
+    }
+
+    const toggleDisplayIsList = () => {
+      setDisplayIsList(!displayIsList);
+    }
+
+
+    const toggleMenuIsOpen = () => {
+      setMenuIsOpen(!menuIsOpen);
+    }
+
+    //ローカルストレージのキーのstoredMemosに追加される
+    const updateStoredMemos = (updatedMemos: Memo[]) => {
+      localStorage.setItem('storedMemos', JSON.stringify(updatedMemos));
+    }
+
+
+    return(
+      <>
+        <Header toggleMenuIsOpen={toggleMenuIsOpen} toggleDisplayIsList={toggleDisplayIsList} displayIsList={displayIsList}/>
+        <div className="flex flex-row w-screen">
+          <Drawer menuIsOpen={menuIsOpen}/>
+          <div className="flex flex-col w-full justify-items-center">
+            <InputForm
+              inputText={inputText} onChangeInputText={onChangeInputText} onClick={onClickAdd}
+              inputTitle={inputTitle} onChangeInputTitle={onChangeInputTitle}
+            />
+            <MemoList
+              memoList={memoList}
+              displayIsList={displayIsList}
+              onClickDelete={onClickDelete}/>
+            <MemoDetail />
+          </div>
+        </div>
+      </>
+  );
+}
