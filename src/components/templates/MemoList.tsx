df2cf0ebd68cc8e6ffabfba1ed19d9aa696d8a3c
natsumi <aka.723n@gmail.com>
2022-06-14 18:02:29 +0900

    feat: pinを追加

--- src/components/templates/MemoList.tsx
+++ src/components/templates/MemoList.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Memo } from '../../App';
 import { MemoDetailModal } from '../pages/MemoDetailModal';
+import { BsPin } from 'react-icons/bs';
 
 
 type MemoListProps = {
@@ -36,16 +37,24 @@ export const MemoList = (props: MemoListProps) => {
       <div className={ listWidth() +' grid gap-4 ' + gridCols()} >
         {props.memoList.map((memo: Memo,index:number) => {
           return(
-            <div key={index} onClick={() => openModal(memo)}>
-              <div className="flex flex-col p-2 rounded-md mx-auto my-8 border border-gray-40
-              hover:shadow-md
-              hover:shadow-gray-300 ">
+            <div key={index} >
+              <div onClick={() => openModal(memo)}
+                   className="flex flex-col p-2 rounded-md mx-auto my-8 border border-gray-40
+                   hover:shadow-md
+                   hover:shadow-gray-300 ">
+                <div className="flex justify-between">
                   <div>{memo.title}</div>
+                  <button className=" p-2 group hover:bg-slate-200 rounded-full relative">
+                    <BsPin size='1.2rem'/>
+                    <span className="opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600
+                      group-hover:opacity-100 absolute top-10 -right-4">メモを固定</span>
+                  </button>
+                </div>
                   <div>{memo.body}</div>
-                <button
-                  className="hover:bg-gray-300"
-                  onClick={() => props.onClickDelete(index)} >削除</button>
               </div>
+              <button
+                className="hover:bg-gray-300"
+                onClick={() => props.onClickDelete(index)} >削除</button>
               <MemoDetailModal memo={modalMemo} closeModal={closeModal} />
             </div>
           );
