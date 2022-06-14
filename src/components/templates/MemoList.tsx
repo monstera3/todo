9aed294ae69c0c26eb85704d9fbf7927c85566d4
natsumi <aka.723n@gmail.com>
2022-06-14 18:08:51 +0900

    styleL pinの位置調整

--- src/components/templates/MemoList.tsx
+++ src/components/templates/MemoList.tsx
@@ -47,7 +47,7 @@ export const MemoList = (props: MemoListProps) => {
                   <button className=" p-2 group hover:bg-slate-200 rounded-full relative">
                     <BsPin size='1.2rem'/>
                     <span className="opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600
-                      group-hover:opacity-100 absolute top-10 -right-4">メモを固定</span>
+                      group-hover:opacity-100 absolute top-9 -right-4">メモを固定</span>
                   </button>
                 </div>
                   <div>{memo.body}</div>
