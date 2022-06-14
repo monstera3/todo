fd9f97e369ac340884a029ffb00ffd0f677f71dd
natsumi <aka.723n@gmail.com>
2022-06-14 16:16:04 +0900

    refactor: rename to MemoDetail

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import { TodoTop } from './components/templates/TodoTop';
 import { Routes,Route } from "react-router-dom"
-import { MemoDetail } from './components/pages/MemoDetail';
+import { MemoDetailModal } from './components/pages/MemoDetailModal';
 import { Reminders } from './components/templates/Reminders';
 import { Trash } from './components/templates/Trash';
 
@@ -20,7 +20,7 @@ export const App = (props: {storedMemoList:Memo[]})=>{
     <div className="App">
       <Routes>
         <Route path="/" element={<TodoTop memoList={memoList} setMemoList={setMemoList} />}/>
-        <Route path="/memo" element={<MemoDetail />} />
+        <Route path="/memo" element={<MemoDetailModal />} />
         <Route path="/reminders" element={<Reminders />} />
         <Route path="/trash" element={<Trash />} />
       </Routes>
--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import { InputForm } from '../organisms/InputForm';
 import { MemoList } from './MemoList';
 import { Dialog, Transition } from '@headlessui/react';
-import { MemoDetail } from '../pages/MemoDetail';
+import { MemoDetailModal } from '../pages/MemoDetailModal';
 import { Memo } from '../../App';
 
 
@@ -81,7 +81,7 @@ const toggleDisplayIsList = () => {
               <div className="flex min-h-full items-center justify-center p-4 text-center">
                 <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95">
                   <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                    <MemoDetail />
+                    <MemoDetailModal />
                     <div>
                       <button type="button" onClick={closeModal}>close</button>
                     </div>
