401dc82523366bb2ec44000983556e178ef87e13
natsumi <aka.723n@gmail.com>
2022-06-15 15:02:33 +0900

    feat: add onClickForm

--- src/components/organisms/InputForm.tsx
+++ src/components/organisms/InputForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type InputFormType = {
   inputText: string,
@@ -10,13 +10,20 @@ type InputFormType = {
 
 export const InputForm = (props:InputFormType) => {
   const {inputText,onChangeInputText,onClick,inputTitle,onChangeInputTitle} = props;
+
+  const [inputFormType,setInputFormType] = useState<boolean>(false);
+
+  const onClickForm = () => {
+    setInputFormType(!inputFormType)
+  }
   return(
     <div
       className="flex w-1/2 justify-between px-5 rounded-md
-      mx-auto	border-2 border-gray-300 m-8 shadow-md shadow-gray-200 ">
+      mx-auto	border-2 border-gray-300 m-8 shadow-lg shadow-gray-300 ">
       <div className="flex flex-col">
         <input type="text" value={inputTitle} onChange={onChangeInputTitle}  placeholder="タイトル"/>
         <input type="text" value={inputText} onChange={onChangeInputText} placeholder="メモを入力" />
+        <button onClick={onClickForm}>切り替え</button>
       </div>
       <button onClick={onClick} className=" py-2 px-4 rounded hover:bg-gray-300">作成</button>
     </div>
