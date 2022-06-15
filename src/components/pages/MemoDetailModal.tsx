2d46d366860bc3f48ab3182df4861af67cdc0173
natsumi <aka.723n@gmail.com>
2022-06-15 12:55:31 +0900

    feat: modalにpinを追加

--- src/components/pages/MemoDetailModal.tsx
+++ src/components/pages/MemoDetailModal.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { Memo } from '../../App';
+import { BsPin } from 'react-icons/bs';
 
 
 export const MemoDetailModal = (props:{ memo: Memo|null, closeModal: () => void }) => {
@@ -21,7 +22,14 @@ export const MemoDetailModal = (props:{ memo: Memo|null, closeModal: () => void 
             <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95">
               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <div>
-                  <div>{memo?.title}</div>
+                  <nav className="flex justify-between">
+                    <div>{memo?.title}</div>
+                    <button className=" p-2 group hover:bg-slate-200 rounded-full relative">
+                      <BsPin size='1.2rem'/>
+                      <span className="opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600
+                      group-hover:opacity-100 absolute top-9 -right-4">メモを固定</span>
+                    </button>
+                  </nav>
                   <div>{memo?.body}</div>
                   <button type="button" onClick={closeModal}>close</button>
                 </div>
