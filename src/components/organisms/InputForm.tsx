6e9f9e5712bae8906268b14d7e2ca89a9a2c9368
natsumi <aka.723n@gmail.com>
2022-06-16 20:20:41 +0900

    feat: 閉じるボタンにonClick関数を渡す

--- src/components/organisms/InputForm.tsx
+++ src/components/organisms/InputForm.tsx
@@ -38,26 +38,25 @@ export const InputForm = (props:InputFormType) => {
           {/*//TODO タイトルの文字を連続で入力したい*/}
           <input className="placeholder:text-slate-800 text-base "
             type="text" value={inputTitle} onChange={onChangeInputTitle}  placeholder="タイトル"/>
-          <input className="placeholder:text-slate-800 text-base "
-                 onClick={onClickForm}
-                 type="text"
-                 value={inputText}
-                 onChange={onChangeInputText}
-                 placeholder="メモを入力..." />
           <button className=" p-2 group hover:bg-slate-200 rounded-full relative">
             <BsPin size='1.2rem'/>
             <span className="opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600
                       group-hover:opacity-100 absolute top-9 -right-4">メモを固定</span>
           </button>
         </div>
-        <button onClick={onClick} className=" py-2 px-4 rounded hover:bg-gray-300">作成</button>
+        {/*//TODO メモを入力の文字を連続で入力したい*/}
+        <input className="placeholder:text-slate-800 text-base "
+               type="text"
+               value={inputText}
+               onChange={onChangeInputText}
+               placeholder="メモを入力..." />
         <div className="flex justify-between">
           <button className=" p-2 group hover:bg-gray-200 rounded-full relative">
             <RiInboxArchiveLine size='1.1rem'/>
             <span className="opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600
                       group-hover:opacity-100 absolute top-9 -right-4">アーカイブ</span>
           </button>
-          <button onClick={onClickForm} className="px-4 hover:bg-gray-100">閉じる</button>
+          <button onClick={() => {onClickForm();onClick();}} className="px-4 hover:bg-gray-100">閉じる</button>
         </div>
       </div>
     );
@@ -66,7 +65,7 @@ export const InputForm = (props:InputFormType) => {
   return(
     <>
       <div
-        className="flex w-1/2 justify-between px-5 py-2 rounded-md
+        className="flex w-80 justify-between px-5 py-2 rounded-md
       mx-auto	border-2 border-gray-300 m-8 shadow-lg shadow-gray-300 ">
         <div className="flex flex-col">
           {FormType(inputFormType)}
