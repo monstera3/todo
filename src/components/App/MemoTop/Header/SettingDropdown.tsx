3c847e30f400e8ba3a308f4a66cc2aa8f3c3e986
natsumi <aka.723n@gmail.com>
2022-06-23 20:37:09 +0900

    style: editのアイコンを削除

--- src/components/App/MemoTop/Header/SettingDropdown.tsx
+++ src/components/App/MemoTop/Header/SettingDropdown.tsx
@@ -33,17 +33,7 @@ export default function SettingDropdown() {
                       active ? 'bg-violet-500 text-white' : 'text-gray-900'
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                   >
-                    {active ? (
-                      <EditActiveIcon
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <EditInactiveIcon
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    )}
+
                     Edit
                   </button>
                 )}
