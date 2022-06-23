a4999f8f051afa5a5a63521149cc7c0532239b05
natsumi <aka.723n@gmail.com>
2022-06-23 19:51:09 +0900

    feat: Menu.Buttonに既存の設定ボタンを入れる

--- src/components/App/MemoTop/Header/SettingDropdown.tsx
+++ src/components/App/MemoTop/Header/SettingDropdown.tsx
@@ -1,15 +1,20 @@
 import { Menu, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
+import React, { Fragment } from 'react'
+import { VscSettingsGear } from 'react-icons/vsc';
 
 export default function SettingDropdown() {
   return (
-    <div className="fixed top-16 w-56 text-right">
+    <div className=" text-right">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-            Options
-
+          <Menu.Button className="">
+            <button className=" p-3  group hover:bg-slate-200 rounded-full relative" aria-label="aa">
+              <VscSettingsGear size='1.2rem'/>
+              <span className="invisible opacity-0 px-2 rounded text-[12px] font-bold text-white p-1 bg-slate-600
+          group-hover:visible opacity-100 absolute top-11 right-0.5">設定</span>
+            </button>
           </Menu.Button>
+
         </div>
         <Transition
           as={Fragment}
