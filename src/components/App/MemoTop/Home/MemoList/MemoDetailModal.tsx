c3b8a87d4cb99399381cb9441d921657fc774522
natsumi <aka.723n@gmail.com>
2022-06-30 08:07:33 +0900

    refactor: extract content

--- src/components/App/MemoTop/Home/MemoList/MemoDetailModal.tsx
+++ src/components/App/MemoTop/Home/MemoList/MemoDetailModal.tsx
@@ -13,6 +13,26 @@ export const MemoDetailModal = (props:{memo: Memo|null, closeModal: () => void, 
     return !!memo;
   }
 
+  const content =(memo:Memo) =>{
+    return(
+      <div>
+        <nav className="flex justify-between">
+          <div>{memo?.title}</div>
+          <button className=" p-2 group hover:bg-slate-200 rounded-full relative">
+            { !!memo ? <Pin toggleMemoIsFixed={props.toggleMemoIsFixed} memo={memo} /> : <></> }
+          </button>
+        </nav>
+        <div>
+          {memo?.body}</div>
+        <div className="flex justify-between">
+          { !!memo ? <ArchiveButton toggleMemoIsArchived={props.toggleMemoIsArchived} memo={memo}/> : <></> }
+          { !!memo ? <OtherActionsDropdown toggleMemoIsTrash={props.toggleMemoIsTrash} memo={memo} /> : <></> }
+          <button onClick={() => {closeModal();}} type="button" className="px-4 hover:bg-gray-100">閉じる</button>
+        </div>
+      </div>
+    );
+  }
+
   return(
     <Transition appear show={modalIsOpen()} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={closeModal}>
@@ -23,21 +43,7 @@ export const MemoDetailModal = (props:{memo: Memo|null, closeModal: () => void, 
           <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95">
               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <div>
-                  <nav className="flex justify-between">
-                    <div>{memo?.title}</div>
-                    <button className=" p-2 group hover:bg-slate-200 rounded-full relative">
-                      { !!memo ? <Pin toggleMemoIsFixed={props.toggleMemoIsFixed} memo={memo} /> : <></> }
-                    </button>
-                  </nav>
-                  <div>
-                    {memo?.body}</div>
-                  <div className="flex justify-between">
-                    { !!memo ? <ArchiveButton toggleMemoIsArchived={props.toggleMemoIsArchived} memo={memo}/> : <></> }
-                    { !!memo ? <OtherActionsDropdown toggleMemoIsTrash={props.toggleMemoIsTrash} memo={memo} /> : <></> }
-                    <button onClick={() => {closeModal();}} type="button" className="px-4 hover:bg-gray-100">閉じる</button>
-                  </div>
-                </div>
+                {!!memo ? content(memo): <></>}
               </Dialog.Panel>
             </Transition.Child>
           </div>
