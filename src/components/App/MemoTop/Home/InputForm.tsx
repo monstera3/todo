d7433646f8da2e8aae70c00d571a0ca6abc58b3b
natsumi <aka.723n@gmail.com>
2022-06-30 08:24:06 +0900

    refactor: インデントや改行を揃える

--- src/components/App/MemoTop/Home/InputForm.tsx
+++ src/components/App/MemoTop/Home/InputForm.tsx
@@ -34,16 +34,14 @@ export const InputForm = (props:InputFormType) => {
     );
   }
 
-    const OpenedForm = () => {
-     return(
+  const OpenedForm = () => {
+    return(
       <div>
         <div className="flex justify-between">
-          <input className="placeholder:text-slate-800 text-base "
-            type="text" ref={titleRef} placeholder="タイトル"/>
+          <input className="placeholder:text-slate-800 text-base " type="text" ref={titleRef} placeholder="タイトル"/>
           <button className=" p-2 group hover:bg-slate-200 rounded-full relative">
             <BsPin size='1.2rem'/>
-            <span className="invisible opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600
-                      group-hover:visible opacity-100 absolute top-9 -right-4">メモを固定</span>
+            <span className="invisible opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600 group-hover:visible opacity-100 absolute top-9 -right-4">メモを固定</span>
           </button>
         </div>
         <input className="placeholder:text-slate-800 text-base "
@@ -54,13 +52,11 @@ export const InputForm = (props:InputFormType) => {
         <div className="flex justify-between">
           <button className=" p-2 group hover:bg-gray-200 rounded-full relative">
             <RiInboxArchiveLine size='1.1rem'/>
-            <span className="invisible opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600
-                      group-hover:visible opacity-100 absolute top-9 -right-4">アーカイブ</span>
+            <span className="invisible opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600 group-hover:visible opacity-100 absolute top-9 -right-4">アーカイブ</span>
           </button>
           <button className=" p-2 group hover:bg-gray-200 rounded-full relative">
             <BsThreeDotsVertical size='1.1rem'/>
-            <span className="invisible opacity-0 py-1 w-[120px] rounded text-[12px] font-bold text-white  bg-slate-600
-                      group-hover:visible opacity-100 absolute top-9 -right-4">その他のアクション</span>
+            <span className="invisible opacity-0 py-1 w-[120px] rounded text-[12px] font-bold text-white  bg-slate-600 group-hover:visible opacity-100 absolute top-9 -right-4">その他のアクション</span>
           </button>
           <button onClick={() => {toggleIsLargeForm();close();}} className="px-4 hover:bg-gray-100">閉じる</button>
         </div>
