1dd4f3d8e5b8a3012004d49ee691c33e97d237ab
natsumi <aka.723n@gmail.com>
2022-06-29 14:19:37 +0900

    style: アーカイブのcss調整

--- src/components/App/MemoTop/Home/MemoList/MemoList.tsx
+++ src/components/App/MemoTop/Home/MemoList/MemoList.tsx
@@ -58,8 +58,8 @@ export const MemoList = (props: MemoListProps) => {
                   <button onClick={()=> props.toggleMemoIsArchived(memo.id)}
                     className=" p-2 group hover:bg-gray-200 rounded-full relative">
                     {memo.isArchived ? <RiInboxUnarchiveLine size='1.1rem'/> : <RiInboxArchiveLine size='1.1rem'/>}
-                    <span className="invisible opacity-0 py-1 w-[70px] rounded text-[12px] font-bold text-white  bg-slate-600
-                      group-hover:visible opacity-100 absolute top-9 -right-4">
+                    <span className="invisible opacity-0 p-1 w-max rounded text-[12px] font-bold text-white  bg-slate-600
+                      group-hover:visible opacity-100 absolute top-[100%] -translate-x-1/2">
                       {memo.isArchived ? 'アーカイブを解除します' : 'アーカイブ'}
                       </span>
                   </button>
