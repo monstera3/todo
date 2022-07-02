245228341b414a0b6c39b7cb3b837ff4c31238f2
natsumi <aka.723n@gmail.com>
2022-07-02 17:16:31 +0900

    style(SearchBar): focus-withinを追加して入力中のcssを変更

--- src/components/App/MemoTop/Header/Header.tsx
+++ src/components/App/MemoTop/Header/Header.tsx
@@ -54,11 +54,11 @@ const DisplayTypeMessage = (isList: boolean) => {
 
 const SearchBar = () => {
   return(
-    <div className="flex box px-2 w-1/2 rounded-md mx-auto bg-slate-100">
+    <form className="flex box px-2 w-1/2 rounded-md mx-auto bg-slate-100 focus-within:bg-white focus-within:shadow-sm focus-within:shadow-gray-400">
       <button className="m-1 p-2 hover:bg-slate-200 rounded-full" ><AiOutlineSearch  size='1.3rem'/></button>
       <input type="text" placeholder="検索" className=" w-full mx-1 "/>
       <button className="m-1 p-2 hover:bg-slate-200 rounded-full "><GrClose size='1rem'/></button>
-    </div>
+    </form>
   );
 
 }
