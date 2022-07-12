0fbf960e9f06a86b695ee8eb8fe966b13b81c29f
natsumi <aka.723n@gmail.com>
2022-07-12 10:59:52 +0900

    refactor(App): format codes
    memo
    option+command+l

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import { MemoTop } from './MemoTop/MemoTop';
-import { Routes,Route } from "react-router-dom"
+import { Routes, Route } from 'react-router-dom'
 import { Reminders } from './Reminders/Reminders';
 import { Trash } from './Trash/Trash';
 import { Archive } from './Archive/Archive';
 import { Home } from './Home/Home';
-
 
 
 export type Memo = {
@@ -19,17 +18,17 @@ export type Memo = {
   isTrashed: boolean
 }
 
-export const App = (props: {storedMemoList:Memo[]})=>{
-  const [memoList,setMemoList] = useState<Memo[]>(props.storedMemoList);
+export const App = (props: { storedMemoList: Memo[] }) => {
+  const [memoList, setMemoList] = useState<Memo[]>(props.storedMemoList);
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<MemoTop memoList={memoList} setMemoList={setMemoList} />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/reminders" element={<Reminders />} />
-          <Route path="/trash" element={<Trash />} />
-          <Route path="/archive" element={<Archive />} />
+        <Route path="/" element={<MemoTop memoList={memoList} setMemoList={setMemoList}/>}>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/reminders" element={<Reminders/>}/>
+          <Route path="/trash" element={<Trash/>}/>
+          <Route path="/archive" element={<Archive/>}/>
         </Route>
       </Routes>
     </div>
