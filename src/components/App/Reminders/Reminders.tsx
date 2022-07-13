75e9b5e427935faa8b90e7a4a57c309de0806155
natsumi <aka.723n@gmail.com>
2022-07-13 17:45:22 +0900

    feat(Reminders): リマインダーの初期表示

--- src/components/App/Reminders/Reminders.tsx
+++ src/components/App/Reminders/Reminders.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import { BiBell } from 'react-icons/bi';
 
 
 export const Reminders = () => {
   return(
-    <div>Reminders</div>
+    <div className="m-auto flex flex-col items-center">
+      <BiBell className='m-8 opacity-20 content-center' size='5rem'/>
+      <p className="text-[1.4rem] text-gray-400 font-medium">今後のリマインダーが設定されているメモはここに表示されます</p>
+    </div>
   );
 }
