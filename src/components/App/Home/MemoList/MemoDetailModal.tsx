0887dbf9fa7bef946707373749d1e40124e85ece
natsumi <aka.723n@gmail.com>
2022-07-19 13:44:18 +0900

    docs: add TODO

--- src/components/App/Home/MemoList/MemoDetailModal.tsx
+++ src/components/App/Home/MemoList/MemoDetailModal.tsx
@@ -27,6 +27,7 @@ export const MemoDetailModal = (props:MemoDetailModalType) => {
       <div>
         <nav className="flex justify-between">
           <div>{memo.title}</div>
+            {/*TODO 枠外のツールチップが表示されない*/}
             <PinButton toggleMemoIsFixed={props.toggleMemoIsFixed} memo={memo} />
         </nav>
         <div>{memo.body}</div>
@@ -49,7 +50,7 @@ export const MemoDetailModal = (props:MemoDetailModalType) => {
     <Transition appear show={modalIsOpen()} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={closeModal}>
         <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
+          <div className="fixed inset-0 bg-black bg-opacity-25 " />
         </Transition.Child>
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
