2c36b0a362ed1df82a875ce6d9fb19263cfe4c7b
natsumi <aka.723n@gmail.com>
2022-07-20 14:31:16 +0900

    feat(EditLabelModal): isOpenの初期値をfalseに変更

--- src/components/shared/atoms/EditLabelModal.tsx
+++ src/components/shared/atoms/EditLabelModal.tsx
@@ -2,7 +2,7 @@ import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 
 export  const EditLabelModal = () => {
-  let [isOpen, setIsOpen] = useState(true)
+  let [isOpen, setIsOpen] = useState(false)
 
   function closeModal() {
     setIsOpen(false)
