3180125e58ea69f4fc5bec2e8c391d48fc7e3698
natsumi <aka.723n@gmail.com>
2022-07-29 10:24:17 +0900

    docs: add TODO

--- src/components/App/Home/MemoList/MemoList.tsx
+++ src/components/App/Home/MemoList/MemoList.tsx
@@ -39,7 +39,7 @@ export const MemoList = (props: MemoListProps) => {
   }
 
 
-
+  //TODO 何番目を変更するか検知させたい
   const tailColor = (colorName: string) => {
     const color = colorList.find((c) => c.name === colorName);
     return color ? color.tailName : 'bg-red-200';
